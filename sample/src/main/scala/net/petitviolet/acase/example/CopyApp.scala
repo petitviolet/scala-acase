57f76ce22c154a73439c63de27ad48908e67be6c
petitviolet <violethero0820@gmail.com>
2017-03-18 15:19:27 +0900

    add @Copy sample

---
+++ sample/src/main/scala/net/petitviolet/acase/example/CopyApp.scala
@@ -0,0 +1,16 @@
+package net.petitviolet.acase.example
+
+import net.petitviolet.meta.acase.{Copy, ToString}
+
+@ToString @Copy
+class CopyApp(val n: Int, val s: String)
+
+object CopyAppApp extends App {
+  val c = new CopyApp(10, "hoge")
+  c.toString
+  assert(c.copy().n == 10)
+  assert(c.copy(n = 100).n == 100)
+  assert(c.copy(s = "foo").s == "foo")
+  val x = c.copy(n = 99, s = "foo")
+  assert(x.n == 99 && x.s == "foo")
+}
