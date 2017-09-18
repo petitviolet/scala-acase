e93e61aedda43b31afdbb2e99ad3f55f7e8e6f1b
petitviolet <violethero0820@gmail.com>
2017-09-18 23:17:41 +0900

    @NoApply sample added

--- sample/src/main/scala/net/petitviolet/acase/example/ApplyApp.scala
+++ sample/src/main/scala/net/petitviolet/acase/example/ApplyApp.scala
@@ -1,10 +1,17 @@
 package net.petitviolet.acase.example
 
-import net.petitviolet.meta.acase.Apply
+import net.petitviolet.meta.acase.{Apply, NoApply}
 
 @Apply
 class ApplyApp(value: Int)
 
 object ApplyAppApp extends App {
   println(ApplyApp(100))
+}
+
+@NoApply case class NoApplyTarget private (value: Int)
+
+object NoApplyTargetApp extends App {
+  // cannot compile
+//  println(NoApplyTarget(100))
 }
